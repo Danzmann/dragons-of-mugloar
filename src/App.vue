75ed163e069344568898fdc0a571d9d7cc027e78
Nathan <nathan.danzmann@gmail.com>
2019-06-20 14:45:37 +0300

    Create main css structure for app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
 <template>
 	<div id="app">
-		<div class="header">
-			<img alt="Dragons of Mulogar" src="./assets/logo.png">
-		</div>
-		<HelloWorld msg="Welcome to Your Vue.js App"/>
+        <!--<div class="app-wrapper">-->
+            <div class="header">
+                <img alt="Dragons of Mulogar" src="./assets/logo.png">
+            </div>
+            <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <!--</div>-->
 	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "app",
+    name: 'app',
 
-	components: {
-		HelloWorld,
-	}
+    // internal state is function, to save memory?
+    data: () => ({
+        isStarted: false,
+    }),
+
+    components: {
+        HelloWorld,
+    },
 };
 </script>
 
-<style>
-#app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+<style lang="scss">
+html {
+    height: calc(100% - 16px);
+    body {
+        height: 100%;
+        #app {
+            height: 100%;
+
+            font-family: "MedievalSharp";
+            background-image: linear-gradient(
+                    rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)
+                ), url('./assets/background.jpg');
+            background-size: cover;
+
+            .header {
+                text-align: center;
+                color: #2c3e50;
+            }
+
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: center;
+        }
+    }
 }
 </style>
